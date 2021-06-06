0a7b8b0d8d278014596694fc445b46cf3c393c68
rachelsch1 <schwarz.rachel.e@gmail.com>
2021-06-06 15:08:29 -0400

    initial solution

--- src/main/java/ex15/base/ex15.java
+++ src/main/java/ex15/base/ex15.java
@@ -1,4 +1,20 @@
 package ex15.base;
 
+import java.util.Scanner;
+
 public class ex15 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // password is Qwerty$9
+        System.out.print("What is the password? ");
+        String inputPass = input.nextLine();
+
+        if(inputPass.equals("Qwerty$9")) {
+            System.out.println("Welcome!");
+        }
+        else {
+            System.out.println("I don't know you.");
+        }
+    }
 }
